688c41cc20d5c6d6a379c7b83fd7bb826d1dce46
Asaf Baibekov <asafall21@gmail.com>
2020-08-09 17:45:21 +0300

    add token system to users endpoints

--- routes/users.js
+++ routes/users.js
@@ -1,34 +1,36 @@
 var express = require('express');
 var router = express.Router();
 var validator = require('validator')
-var User = require('../schemes/user')
+var User = require('../schemas/user')
 
 var bcrypt = require('bcrypt');
 const saltRounds = 10;
 
+var jwt = require('jsonwebtoken')
+
 router.post('/register', async function(req, res, next) {
     let { name, email, password, mobile, birth_date, gender } = req.body
-    if (name == null) res.send({ code: 1, description: 'name required' })
-    else if (typeof name != 'string') res.send({ code: 1, description: 'name must be string' })
-    else if (email == null) res.send({ code: 1, description: 'email required' })
-    else if (typeof email != 'string') res.send({ code: 1, description: 'email must be string' })
-    else if (!validator.isEmail(email)) res.send({ code: 1, description: 'email must be valid' })
-    else if (password == null) res.send({ code: 1, description: 'password required' })
-    else if (typeof password != 'string') res.send({ code: 1, description: 'password must be string' })
-    else if (mobile == null) res.send({ code: 1, description: 'mobile required' })
-    else if (typeof mobile != 'string') res.send({ code: 1, description: 'mobile must be string' })
-    else if (!validator.isMobilePhone(mobile)) res.send({ code: 1, description: 'mobile must be valid' })
-    else if (birth_date == null) res.send({ code: 1, description: 'birth_date required' })
-    else if (typeof birth_date != 'object') { console.log(typeof birth_date); res.send({ code: 1, description: 'birth_date must be object' }) }
-    else if (birth_date.day == null) res.send({ code: 1, description: 'birth_date.day required' })
-    else if (typeof birth_date.day != 'number') res.send({ code: 1, description: 'birth_date.day must be number' })
-    else if (birth_date.month == null) res.send({ code: 1, description: 'birth_date.month required' })
-    else if (typeof birth_date.month != 'number') res.send({ code: 1, description: 'birth_date.month must be number' })
-    else if (birth_date.year == null) res.send({ code: 1, description: 'birth_date.year required' })
-    else if (typeof birth_date.year != 'number') res.send({ code: 1, description: 'birth_date.year must be number' })
-    else if (gender == null) res.send({ code: 1, description: 'gender required' })
-    else if (typeof gender != 'string') res.send({ code: 1, description: 'gender must be string' })
-    else if (gender != 'male' && gender != 'female') res.send({ code: 1, description: 'gender must be male of female' })
+    if (name == null) res.send({ code: 2, description: 'name required' })
+    else if (typeof name != 'string') res.send({ code: 2, description: 'name must be string' })
+    else if (email == null) res.send({ code: 2, description: 'email required' })
+    else if (typeof email != 'string') res.send({ code: 2, description: 'email must be string' })
+    else if (!validator.isEmail(email)) res.send({ code: 2, description: 'email must be valid' })
+    else if (password == null) res.send({ code: 2, description: 'password required' })
+    else if (typeof password != 'string') res.send({ code: 2, description: 'password must be string' })
+    else if (mobile == null) res.send({ code: 2, description: 'mobile required' })
+    else if (typeof mobile != 'string') res.send({ code: 2, description: 'mobile must be string' })
+    else if (!validator.isMobilePhone(mobile)) res.send({ code: 2, description: 'mobile must be valid' })
+    else if (birth_date == null) res.send({ code: 2, description: 'birth_date required' })
+    else if (typeof birth_date != 'object') { console.log(typeof birth_date); res.send({ code: 2, description: 'birth_date must be object' }) }
+    else if (birth_date.day == null) res.send({ code: 2, description: 'birth_date.day required' })
+    else if (typeof birth_date.day != 'number') res.send({ code: 2, description: 'birth_date.day must be number' })
+    else if (birth_date.month == null) res.send({ code: 2, description: 'birth_date.month required' })
+    else if (typeof birth_date.month != 'number') res.send({ code: 2, description: 'birth_date.month must be number' })
+    else if (birth_date.year == null) res.send({ code: 2, description: 'birth_date.year required' })
+    else if (typeof birth_date.year != 'number') res.send({ code: 2, description: 'birth_date.year must be number' })
+    else if (gender == null) res.send({ code: 2, description: 'gender required' })
+    else if (typeof gender != 'string') res.send({ code: 2, description: 'gender must be string' })
+    else if (gender != 'male' && gender != 'female') res.send({ code: 2, description: 'gender must be male of female' })
     else {
     try {
         let salt = await bcrypt.genSalt(saltRounds);
@@ -39,18 +41,74 @@ router.post('/register', async function(req, res, next) {
             password: hashed_password,
             mobile,
             birth_date: {
-            day: birth_date.day,
-            month: birth_date.month,
-            year: birth_date.year
+                day: birth_date.day,
+                month: birth_date.month,
+                year: birth_date.year
             },
             gender: { enum: [ gender ] }
         }).save()
             .then(doc => { res.send({ code: 0, description: "success" }) })
-            .catch(err => { res.send({ code: 2, description: "user already exist"}) })
+            .catch(err => { res.send({ code: 3, description: "user already exist"}) })
         } catch (err) {
-        console.error(err)
+            res.send({ code: 1, description: 'unknown error' })
         }
     }
+})
+
+function generateAccessToken(email) {
+    return jwt.sign({ email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15s' })
+}
+
+router.post('/login', async function(req, res, next) {
+    let { email, password } = req.body
+    if (email == null) res.send({ code: 2, description: 'email required' })
+    else if (typeof email != 'string') res.send({ code: 2, description: 'email must be string' })
+    else if (!validator.isEmail(email)) res.send({ code: 2, description: 'email must be valid' })
+    else if (password == null) res.send({ code: 2, description: 'password required' })
+    else if (typeof password != 'string') res.send({ code: 2, description: 'password must be string' })
+    else {
+        User.findOne({ email }, async (err, user) => {
+            if (err != null) return res.sendStatus(500)
+            if (await bcrypt.compare(password, user.password)) {
+                let access_token = generateAccessToken(email)
+                let refresh_token = jwt.sign({ email }, process.env.REFRESH_TOKEN_SECRET)
+                User.findByIdAndUpdate(user._id, { access_token, refresh_token }, (err) => {
+                    if (err != null) return res.sendStatus(500)
+                    res.send({ accessToken: access_token, refreshToken: refresh_token })
+                })
+            } else {
+                res.send({ code: 4, description: 'not authenticated'})
+            }
+        })
+    }
+})
+
+router.post('/token', (req, res) => {
+    let refresh_token = req.body.token
+    if (refresh_token == null) return res.sendStatus(401)
+    User.findOne({ refresh_token }, (err, user) => {
+        if (err != null) return res.sendStatus(403)
+        jwt.verify(user.refresh_token, process.env.REFRESH_TOKEN_SECRET, (err, payload) => {
+            if (err) return res.sendStatus(403)
+            let access_token = generateAccessToken(payload.email)
+            User.findOneAndUpdate({ email: payload.email }, { access_token }, (err) => {
+                if (err != null) return res.sendStatus(500)
+                res.send({ accessToken: access_token })
+            })
+        })
+    })
+})
+
+router.delete('/logout', (req, res) => {
+    let refresh_token = req.body.token
+    User.findOne({ refresh_token }, (err, user) => {
+        if (err != null) return res.sendStatus(403)
+        if (user == null) return res.send({ code: 5, description: 'user not found'})
+        User.findByIdAndUpdate(user._id, { access_token: '', refresh_token: '' }, (err) => {
+            if (err != null) return res.sendStatus(500)
+            res.send({ code: 0, description: 'success' })
+        })
+    })
 })
 
 module.exports = router;
