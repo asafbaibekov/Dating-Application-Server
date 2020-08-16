1ac7c01e16b8b827b309afb29d98ef2a383f3580
Asaf Baibekov <asafall21@gmail.com>
2020-08-16 17:44:06 +0300

    add valid token conditions

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,12 @@ var jwt = require('jsonwebtoken')
 
 module.exports = function (req, res, next) {
     var token = req.header('Authorization')
+    if (token == null)
+        return res.status(401).send({ code: 1, description: 'token required' })
+    if (typeof token != 'string')
+        return res.status(401).send({ code: 1, description: 'token must be string' })
     if (token.split(' ')[0] !== 'Bearer' || token.split(' ')[1] == null)
-        return res.statusCode(401).send({ code: 1, description: 'token required' })
+        return res.status(401).send({ code: 1, description: 'token required' })
     var token = token.split(' ')[1]
     try {
         const payload = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
