b69cefad8281e915bb3bf6ac6d338836245bfda4
Asaf Baibekov <asafall21@gmail.com>
2020-10-04 17:25:30 +0300

    create endpoint to get all user's data

--- routes/profile.js
+++ routes/profile.js
@@ -1,4 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+const User = require("../schemas/user")
+
+router.get('/me', (req, res) => {
+    User.findById(req.user_id, '-password -access_token -refresh_token -is_mobile_verified').orFail()
+        .then(user => {
+            res.send({ code: 0, description: 'success', user })
+        })
+        .catch(error => {
+            if (error.name == 'DocumentNotFoundError')
+                res.send({ code: 5, description: 'user_id not found' })
+            else
+                res.send({ code: 1, description: "unknown error" })
+        })
+})
+
 module.exports = router;
