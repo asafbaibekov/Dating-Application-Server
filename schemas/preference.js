96ffe920d05775be92e64b68a97320bd4ce4fd72
Asaf Baibekov <asafall21@gmail.com>
2020-10-11 22:21:44 +0300

    fix preference validation

--- schemas/preference.js
+++ schemas/preference.js
@@ -6,21 +6,13 @@ var preferenceSchema = new mongoose.Schema({
         type: Number,
         min: 18,
         max: 80,
-        required: true,
-        validate: {
-            validator: function(value) { return this.max_age >= value },
-            message: 'min_age has to be lower than or equal to max_age'
-        }
+        required: true
     },
     max_age: {
         type: Number,
         min: 18,
         max: 80,
-        required: true,
-        validate: {
-            validator: function(value) { return this.min_age <= value },
-            message: 'max_age has to be greater than or equal to min_age'
-        }
+        required: true
     },
     distance: {
         type: Number,
@@ -38,22 +30,14 @@ var preferenceSchema = new mongoose.Schema({
         min: 140,
         max: 250,
         default: 140,
-        required: true,
-        validate: {
-            validator: function(value) { return this.max_height >= value },
-            message: 'min_height has to be lower than or equal to max_height'
-        }
+        required: true
     },
     max_height: {
         type: Number,
         min: 140,
         max: 250,
         default: 250,
-        required: true,
-        validate: {
-            validator: function(value) { return this.min_height <= value },
-            message: 'max_height has to be greater than or equal to min_height'
-        }
+        required: true
     },
     interests: {
         type: [String],
@@ -72,5 +56,73 @@ var preferenceSchema = new mongoose.Schema({
         enum: ['over_weight', 'medium', 'shapely', 'skinny']
     }
 });
+
+preferenceSchema.pre('save', function(next) {
+    if (this.min_age > this.max_age) {
+        let age_error = new mongoose.Error.ValidationError()
+        age_error.message = `min_age (${this.min_age}) has to be lower than or equal to max_age (${this.max_age})`
+        next(age_error)
+    } else if (this.min_height > this.max_height) {
+        let height_error = new mongoose.Error.ValidationError()
+        height_error.message = `min_height (${this.min_height}) has to be lower than or equal to max_height (${this.max_height})`
+        next(height_error)
+    } else next()
+})
+
+preferenceSchema.pre('findOneAndUpdate', function(next) {
+    if (this._update.$set == undefined) next()
+    let min_age = this._update.$set.min_age
+    let max_age = this._update.$set.max_age
+    if (!min_age && !max_age) { next(); return; }
+    else if (min_age && max_age) {
+        if (min_age <= max_age) { next(); return; }
+        let age_error = new mongoose.Error.ValidationError()
+        age_error.message = `min_age (${min_age}) has to be lower than or equal to max_age (${max_age})`
+        next(age_error)
+        return;
+    }
+    this.model.findOne(this._conditions)
+        .then(preference => {
+            let age_error = new mongoose.Error.ValidationError()
+            if (min_age && min_age > preference.max_age) {
+                age_error.message = `min_age (${min_age}) has to be lower than or equal to max_age (${preference.max_age})`
+                next(age_error)
+            } else if (max_age && preference.min_age > max_age) {
+                age_error.message = `min_age (${preference.min_age}) has to be lower than or equal to max_age (${max_age})`
+                next(age_error)
+            } else {
+                next()
+            }
+        })
+        .catch(next)
+})
+
+preferenceSchema.pre('findOneAndUpdate', function(next) {
+    if (this._update.$set == undefined) { next() }
+    let min_height = this._update.$set.min_height
+    let max_height = this._update.$set.max_height
+    if (!min_height && !max_height) { next(); return; }
+    else if (min_height && max_height) {
+        if (min_height <= max_height) { next(); return; }
+        let height_error = new mongoose.Error.ValidationError()
+        height_error.message = `min_height (${min_height}) has to be lower than or equal to max_height (${max_height})`
+        next(height_error)
+        return;
+    }
+    this.model.findOne(this._conditions)
+        .then(preference => {
+            let height_error = new mongoose.Error.ValidationError()
+            if (min_height && min_height > preference.max_height) {
+                height_error.message = `min_height (${min_height}) has to be lower than or equal to max_height (${preference.max_height})`
+                next(height_error)
+            } else if (max_height && preference.min_height > max_height) {
+                height_error.message = `min_height (${preference.min_height}) has to be lower than or equal to max_height (${max_height})`
+                next(height_error)
+            } else {
+                next()
+            }
+        })
+        .catch(next)
+})
 
 module.exports = mongoose.model('Preference', preferenceSchema);
