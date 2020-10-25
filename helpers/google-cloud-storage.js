b7bc57a3407a92b2f883a88affc09047fa1871fe
Asaf Baibekov <asafall21@gmail.com>
2020-10-25 16:34:52 +0200

    fill /story router with relevant methods

--- helpers/google-cloud-storage.js
+++ helpers/google-cloud-storage.js
@@ -8,6 +8,7 @@ const storage = new Storage({
     keyFilename: path.join(__dirname, '../alanica-dates-storage-credential.json'),
     projectId: 'alanica-dates-291119'
 })
+var stories_bucket = storage.bucket('alanica-dates-stories')
 var messages_bucket = storage.bucket('alanica-dates-291119.appspot.com')
 var profile_pictures_bucket = storage.bucket('alanica-dates-profile-pictures')
 
@@ -69,4 +70,38 @@ exports.upload_profile_picture = (file_path) => {
  */
 exports.delete_profile_picture = (file_name) => {
     return profile_pictures_bucket.file(file_name).delete()
+}
+
+/**
+ * Get signed URL after upload file to story bucket.
+ * @param {string} file_path
+ * @return {Promise<String>}
+ */
+exports.upload_story_file = (file_path) => {
+    let file = stories_bucket.file(uuidv4())
+    return new Promise((resolve, reject) => {
+        fs.createReadStream(file_path)
+            .pipe(file.createWriteStream({ resumable: false, gzip: true }))
+            .on('error', reject)
+            .on('finish', () => {
+                file.getSignedUrl({ action: 'read', expires: Date.now() + 60 * 60 * 24 * 12 })
+                    .then((urls) => {
+                        resolve({
+                            name: file.name,
+                            bucket: file.bucket.name,
+                            url: urls[0]
+                        })
+                    })
+                    .catch(reject);
+            })
+    })
+}
+
+/**
+ * deleting files from stories bucket by giving an array of file's names
+ * @param {Array<String>} files_names
+ * @return {Array<Promise<any>>}
+ */
+exports.delete_story_files = (files_names) => {
+    return Promise.all(files_names.map(name => stories_bucket.file(name).delete()))
 }
--- routes/story.js
+++ routes/story.js
@@ -1,4 +1,84 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require("../schemas/user")
+const Story = require("../schemas/story")
+const File = require("../schemas/file")
+
+const formidable = require('formidable');
+
+var { upload_story_file, delete_story_files } = require('../helpers/google-cloud-storage')
+
+router.use((req, res, next) => {
+    let date = new Date()
+    date.setHours(date.getHours() - 12)
+    File.find({ bucket: "alanica-dates-stories", createdAt: { $lt: date } })
+        .then(files => {
+            return delete_story_files(files.map(file => file.name))
+                .finally(() => {
+                    return Promise.all(files.map(file => File.findByIdAndRemove(file._id)))
+                        .then((files_removed) => Promise.all(files_removed.map(file => Story.findOneAndUpdate({ files: file._id }, { $pull: { files: file._id } }))))
+                })
+        })
+        .then(() => { next() })
+        .catch(next)
+})
+
+router.get('/me', (req, res) => {
+    Story.findOne({ user: req.user_id })
+        .then(story => {
+            res.send({ code: 0, description: 'success', story })
+        })
+        .catch(error => {
+            res.send({ code: 1, description: 'unknown error' })
+        })
+})
+
+router.post('/add', (req, res) => {
+    var form = new formidable.IncomingForm({ keepExtensions: true, maxFileSize: 10 * 1024 * 1024 });
+    User.findById(req.user_id, '_id').orFail()
+        .then(user => {
+            return new Promise((resolve, reject) => {
+                form.parse(req, (error, fields, files) => {
+                    if (error) { error.name = 'FormidableError'; reject(error) }
+                    else resolve({ fields, files })
+                })
+            })
+            .then(({ files }) => {
+                if (files.file == null || files.file.type == null) {
+                    let error = new Error()
+                    error.name = 'EmptyFile'
+                    error.message = 'file required'
+                    throw error
+                }
+                if (files.file.type.split('/')[0] != 'image' && files.file.type.split('/')[0] != 'video') {
+                    let error = new Error()
+                    error.name = 'FileTypeError'
+                    error.message = 'file must be image or video type'
+                    throw error
+                }
+                return upload_story_file(files.file.path)
+            })
+            .then(object => File.create(object))
+            .then(file => Story.findOneAndUpdate({ user: user._id }, { $push: { files: file._id } }, { upsert: true, new: true, runValidators: true }).populate('files'))
+            .then(story => {
+                res.send({ code: 0, description: 'success', story })
+            })
+        })
+        .catch(error => {
+            switch (error.name) {
+                case 'DocumentNotFoundError':
+                    res.send({ code: 5, description: 'story not found for user, create story before insert file' })
+                    break
+                case 'EmptyFile':
+                case 'FileTypeError':
+                case 'FormidableError':
+                    res.send({ code: 2, description: error.message })
+                    break
+                default:
+                    res.send({ code: 1, description: 'unknown error' })
+            }
+        })
+})
+
 module.exports = router;
