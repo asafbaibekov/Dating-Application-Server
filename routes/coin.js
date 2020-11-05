2b9d70465103d29658985db44d0af94fbe4c989b
roi.ilan <roiilan23@gmail.com>
2020-11-05 22:23:06 +0200

    /coin/me - create coin and updtae credit coins

--- routes/coin.js
+++ routes/coin.js
@@ -7,16 +7,48 @@ const authenticate = require('./auth')
 
 
 router.get('/me', authenticate.http_auth, (req, res) => {
-    Coin.findOneAndUpdate({ user: req.user_id }, {}, { new: true, upsert: true, runValidators: true, setDefaultsOnInsert: true })
-        .then(coin => {
-            res.send({ code: 0, description: 'success', coin })
-        })
-        .catch(error => { 
-            if (error.name == 'DocumentNotFoundError')
-                res.send({ code: 5, description: error.message })
-            else
-                res.send({ code: 1, description: "unknown error" })
-        })
+    let user = req.user_id
+    Coin.findOneAndUpdate({ user }, {}, { new: true, upsert: true, runValidators: true, setDefaultsOnInsert: true })
+    .then(newCoin => {
+        var incAmount = 1
+      
+    function checkCredit (last_login){
+        const second = 1000; // = 1000 milliseconds 
+        const hour = 3600 * second
+        let creditTime = 24 * hour
+        let currDate = new Date()       
+
+        if (currDate - newCoin.last_login >= creditTime ) return true
+        else return false
+    }
+
+    let isCredit = checkCredit (newCoin.last_login)
+    console.log("isCredit - ", isCredit)
+
+
+        if (isCredit) { // past 24 hours
+            Coin.findByIdAndUpdate({ _id: newCoin._id },  { $inc: { "amount": incAmount }, $set: { "last_login": new Date() }  }, { new: true })
+            .then(updtaeCoin => {
+                res.send({ code: 0, description: 'success to uptade', updtaeCoin })
+            })
+            .catch(error => { 
+                if (error.name == 'DocumentNotFoundError')
+                    res.send({ code: 5, description: error.message })
+                else
+                    res.send({ code: 1, description: error.message })
+            })
+        }
+        else {
+            res.send({ code: 0, description: 'success', newCoin })
+        }              
+    })
+    .catch(error => { 
+        if (error.name == 'DocumentNotFoundError')
+            res.send({ code: 5, description: error.message })
+        else
+            res.send({ code: 1, description: "unknown error on create" })
+    })
 })
+
 
 module.exports = router;
--- schemas/coin.js
+++ schemas/coin.js
@@ -14,7 +14,7 @@ const coinSchema = new mongoose.Schema({
     },
     amount: {
         type: Number,
-        default: 0,
+        default: 10,
         required: true
     }
 })
