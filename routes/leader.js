cfaa3637e6e6ea977aa3b6c42277da7c018e8688
roi.ilan <roiilan23@gmail.com>
2020-11-09 14:20:54 +0200

    make api req that return leader list /leade/list

--- routes/leader.js
+++ routes/leader.js
@@ -10,10 +10,8 @@ router.put('/add', (req, res) => {
     let { description } = req.body
     User.findById(req.user_id).orFail()
         .then(user => {
-            console.log(user._id)
-            Profile.findOne( {user: user._id })
+            Profile.findOne({ user: user._id })
                 .then(profile => {
-                    console.log(profile)
                     Leader.findOneAndUpdate(
                         { profile: profile._id },
                         { profile: profile._id, description },
@@ -22,7 +20,7 @@ router.put('/add', (req, res) => {
                             runValidators: true,
                             upsert: true,
                             setDefaultsOnInsert: true,
-                        })
+                        })                        
                             .then((leader) => {
                                 res.send({ code: 0, description: "success", leader });
                             })
@@ -39,6 +37,13 @@ router.put('/add', (req, res) => {
                                 }
                             })
                 })
+                .catch((error) => {
+                    if (error.message == `Cannot read property '_id' of null`)  // profile is not exist
+                        res.send({ code: 2, description: error.message });
+                    if (error.name == "DocumentNotFoundError")
+                        res.send({ code: 5, description: error.message });
+                    else res.send({ code: 1, description: "unknown error" });
+                });
         })
         .catch((error) => {
             if (error.name == "DocumentNotFoundError")
@@ -48,7 +53,17 @@ router.put('/add', (req, res) => {
 })
 
 router.get('/list', (req, res) => {
-
+    Leader.find({}).sort({ _id: -1 }).limit(25).populate("profile")
+        .then(leaders =>{
+            res.send({ code: 0, description: "success", leaders })           
+        })
+        .catch(error => {
+            if (error.name == "DocumentNotFoundError")
+                res.send({ code: 5, description: error.message });
+            else
+                res.send({ code: 1, description: "unknown error" });
+        });
 })
+
 
 module.exports = router;
