897522d65cfd17bea29b3a5c91e623de2b85c631
Asaf Baibekov <asafall21@gmail.com>
2020-11-12 03:12:34 +0200

    message without files or with one file works

--- routes/messaging.js
+++ routes/messaging.js
@@ -12,51 +12,67 @@ var { upload_message_file } = require('../helpers/google-cloud-storage')
 
 router.post('/message', (req, res) => {
     var form = new formidable.IncomingForm({ keepExtensions: true, maxFileSize: 10 * 1024 * 1024 });
-    form.parse(req, async (error, fields, files) => {
-        if (error) return res.send({ code: 2, description: error.message })
-
-        let { receiver_id, message_text } = fields
-        if (receiver_id == null)
-            return res.send({ code: 2, description: 'receiver_id required' })
-        if (typeof receiver_id != 'string')
-            return res.send({ code: 2, description: 'receiver_id must be string' })
-
-        try {
-            var image_file = null
-            var audio_file = null
-            let { message_image, message_audio } = files
-            if (message_image != null) {
-                if (message_image.type.split('/')[0] != 'image')
-                    return res.send({ code: 2, description: 'message_image must be image file' })
-                let object = await upload_message_file(message_image.path)
-                image_file = await File.create(object)
-            }
-            if (message_audio != null) {
-                if (message_audio.type.split('/')[0] != 'audio')
-                    return res.send({ code: 2, description: 'message_audio must be audio file' })
-                let object = await upload_message_file(message_audio.path)
-                audio_file = await File.create(object)
-            }
-            User.findById(receiver_id).orFail()
-                .then(receiver => Chat.findOne({ users: { $all: [req.user_id, receiver._id] }}))
-                .then(chat => Message.create({ chat_id: chat._id, sender_id: req.user_id, receiver_id: receiver_id, message_text, message_image: image_file._id, message_audio: audio_file._id }))
-                .then(message => message.populate('message_image').execPopulate())
-                .then(message => message.populate('message_audio').execPopulate())
-                .then(message => {
-                    res.send({ code: 0, description: 'success', message })
+    User.findById(req.user_id).orFail()
+        .then(() => {
+            return new Promise((resolve, reject) => {
+                form.parse(req, (error, fields, files) => {
+                    if (error) { error.name = 'FormidableError'; reject(error) }
+                    else resolve({ fields, files })
                 })
-                .catch(err => { 
-                    if (err.name == 'CastError')
-                        res.send({ code: 2, description: 'invalid id' })
-                    else if (err.name == 'DocumentNotFoundError')
-                        res.send({ code: 5, description: 'receiver_id not found' })
-                    else
-                        res.send({ code: 1, description: 'unknown error' })
-                })
-        } catch (error) {
-            res.send({ code: 1, description: "unknown error" })
-        }
-    })
+            })
+            .then(({ fields, files }) => {
+                return Promise.all([
+                    new Promise((resolve) => {
+                        let { receiver_id, message_text } = fields
+                        resolve([receiver_id, message_text])
+                    }),
+                    new Promise((resolve, reject) => {
+                        let { message_image, message_audio } = files
+                        if (message_image != null && message_image.type.split('/')[0] != 'image') {
+                            let error = new Error()
+                            error.name = 'FileTypeError'
+                            error.message = 'message_image must be image file'
+                            reject(error)
+                        }
+                        if (message_audio != null && message_audio.type.split('/')[0] != 'audio') {
+                            let error = new Error()
+                            error.name = 'FileTypeError'
+                            error.message = 'message_audio must be audio file'
+                            reject(error)
+                        }
+                        let image_file = null
+                        let audio_file = null
+                        if (message_image != null)
+                            image_file = upload_message_file(message_image.path).then(object => File.create(object))
+                        if (message_audio != null)
+                            audio_file = upload_message_file(message_audio.path).then(object => File.create(object))
+                        resolve(Promise.all([image_file, audio_file]))
+                    })
+                ])
+            })
+            .then(([[receiver_id, message_text], [image_file, audio_file]]) => {
+                let query = { }
+                if (image_file != null) query.message_image = image_file._id
+                if (audio_file != null) query.message_audio = audio_file._id
+                Object.keys(query).forEach(key => (query[key] == null) && delete query[key]);
+                return User.findById(receiver_id).orFail()
+                    .then(receiver => Chat.findOne({ users: { $all: [req.user_id, receiver._id] }}))
+                    .then(chat => Message.create({ chat_id: chat._id, sender_id: req.user_id, receiver_id: receiver_id, message_text, ...query }))
+            })
+            .then(message => message.populate('message_image').execPopulate())
+            .then(message => message.populate('message_audio').execPopulate())
+            .then(message => {
+                res.send({ code: 0, description: 'success', message })
+            })
+        })
+        .catch(error => { 
+            if (error.name == 'DocumentNotFoundError')
+                res.send({ code: 5, description: 'receiver_id not found' })
+            else if (error.name == 'FileTypeError')
+                res.send({ code: 2, description: error.message })
+            else
+                res.send({ code: 1, description: 'unknown error' })
+        })
 });
 
 router.get('/chats', (req, res) => {
